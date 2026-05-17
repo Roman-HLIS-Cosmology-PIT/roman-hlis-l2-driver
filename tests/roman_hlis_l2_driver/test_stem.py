d6f3db579465c0c86f884f475c5ed94105e8e706
hirata10 <hirata.10@osu.edu>
2026-05-17 14:52:37 -0400

    ruff reformat

--- tests/roman_hlis_l2_driver/test_stem.py
+++ tests/roman_hlis_l2_driver/test_stem.py
@@ -1,8 +1,8 @@
 """Test for file name utilities."""
 
 import pytest
-
 from roman_hlis_l2_driver.name_util import stem_l2
+
 
 def test_stem():
     """Test the stem_l2 function."""
@@ -11,4 +11,4 @@ def test_stem():
     assert f == "/sim_L2_J129"
 
     with pytest.raises(ValueError):
-        g = stem_l2("undefined_format", "J129")
+        stem_l2("undefined_format", "J129")
