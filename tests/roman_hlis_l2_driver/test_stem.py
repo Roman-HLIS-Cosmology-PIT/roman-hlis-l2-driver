412bc3c57d73af0dff014892a30b544308dcfc7a
hirata10 <hirata.10@osu.edu>
2026-05-18 14:27:35 -0400

    added unit test for ffov (#28)
    * added unit test for ffov
    
    * added python 3.14 to testing-and-coverage.yml
    
    * added python 3.14 to the smoke-test.yml
    
    * added one more test for FullFoVImageFromFile in test_ffov.py
    
    * fixed import in test
    
    * fixed subscripting of FullFoVImageFromFile object
    
    * deepcopy in FullFoV from file
    
    We don't currently use this feature in the pipeline, but we want to make sure it does what we expect.
    
    * added test_stem.py
    
    * ruff reformat
    
    ---------
    
    Co-authored-by: Christopher Hirata <christopherhirata@Christophers-MacBook-Air.local>

---
+++ tests/roman_hlis_l2_driver/test_stem.py
@@ -0,0 +1,14 @@
+"""Test for file name utilities."""
+
+import pytest
+from roman_hlis_l2_driver.name_util import stem_l2
+
+
+def test_stem():
+    """Test the stem_l2 function."""
+
+    f = stem_l2("L2_2506", "J129")
+    assert f == "/sim_L2_J129"
+
+    with pytest.raises(ValueError):
+        stem_l2("undefined_format", "J129")
