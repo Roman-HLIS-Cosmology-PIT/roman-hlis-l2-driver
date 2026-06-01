9655bba996f807c1adab8784bf9636293d015fc2
hirata10 <hirata.10@osu.edu>
2026-06-01 09:24:48 -0400

    fixed access to original file

--- src/roman_hlis_l2_driver/destripe_interface/destripe.py
+++ src/roman_hlis_l2_driver/destripe_interface/destripe.py
@@ -122,8 +122,9 @@ def destripe_one_layer(cfg_file, noiseid=None, verbose=False):
         else:
             with fits.open(dsout + fp[1] + ".fits") as f:
                 with asdf.open(fp[0][:-5] + "_noise.asdf", memmap=True) as anoise_in:
-                    arr = anoise_in["noise"]
-                    arr[noiseid, :, :] = (f[0].data - a_in["destripe_orig"]).astype(np.float16)
+                    with asdf.open(fp[0], memmap=True) as aorig:
+                        arr = anoise_in["noise"]
+                        arr[noiseid, :, :] = (f[0].data - aorig["destripe_orig"]).astype(np.float16)
             if noiseid == n_noise_layer - 1:
                 # last noise layer
                 with asdf.open(fp[0], mode="r", lazy_load=False) as a:
