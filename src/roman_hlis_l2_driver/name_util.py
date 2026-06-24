b1d80db8a1b72dee27f8eeba26374c7df3879e6a
Katherine Laliotis <klaliotis@cardinal-rw01.ten.osc.edu>
2026-06-24 16:21:52 -0400

    Added file format check for mask files

--- src/roman_hlis_l2_driver/name_util.py
+++ src/roman_hlis_l2_driver/name_util.py
@@ -5,7 +5,7 @@ Utilities for file names and formats.
 
 def stem_l2(file_format, filter):
     """
-    Utility to return the stem format for an L2 file.
+    Utility to return the stem format for a JWST file.
 
     Paramters
     ---------
@@ -23,6 +23,31 @@ def stem_l2(file_format, filter):
 
     if file_format == "L2_2506":
         return f"/sim_L2_{filter:s}"
+    else:
+        print("Please add the new format to name_util.")
+        raise ValueError(f"unsupported format in name_util: {file_format}")
+
+
+def stem_mask(file_format):
+    """
+    Utility to return the stem format for a mask file.
+
+    Paramters
+    ---------
+    file_format : str
+        The format type (same as used in PyIMCOM).
+    filter : str
+        The 4-character filter code.
+
+    Returns
+    -------
+    str
+        A string to append to a directory to get the stem.
+
+    """
+
+    if file_format == "L2_2506":
+        return f"_mask.fits"
     else:
         print("Please add the new format to name_util.")
         raise ValueError(f"unsupported format in name_util: {file_format}")
