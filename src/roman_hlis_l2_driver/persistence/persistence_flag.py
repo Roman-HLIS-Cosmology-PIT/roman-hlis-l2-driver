48c56df3da0c5db04719e5cd7ac6286cd31cf5dc
THT <43194082+aeharbo@users.noreply.github.com>
2026-08-03 14:07:04 -0400

    Refactor previous_obsid function parameters and logic
    Refactor previous_obsid function to accept row_number instead of obsid. Adjust logic to handle cases for row_number and obsid separately.

--- src/roman_hlis_l2_driver/persistence/persistence_flag.py
+++ src/roman_hlis_l2_driver/persistence/persistence_flag.py
@@ -4,31 +4,48 @@
 import numpy as np
 from astropy.io import fits as f
 
-def previous_obsid(obsid: int, inputfile: str):
+def previous_obsid(obsid: None, row_number: int, inputfile: str):
 	""" Given an observation ID and a path to a FITS file,
         returns the ID of the observation that came before it in the file.
     Parameters
     ---------
     obsid: int
         Observation ID, taken as the modified Julian date.
+    row_number: int
+        Row number, or index, of an observation within the fits file.
     inputfile: str or str-like
         Path to observation table fits file.
     Returns
     -------
     obsid_prev: int
         Observation ID of the previous observation to the one passed in.
+    previous_index: int
+        Row number of the observation previous to the row_number passed.
 	"""
 	in_file = f.open(inputfile)
-	
-	all_obsid = in_file[1].data["date"]
-	sorted_obsid_indices = np.argsort(all_obsid)
-	
-	previous_obsid = None
+	all_obs = in_file[1].data["date"]
 
-	for current_index in sorted_obsid_indices:
-		current_obsid = all_obsid[current_index]
-		if current_obsid >= obsid:
-			break
-		previous_obsid = float(current_obsid)
+	if row_number is not None and obsid is not None:
+		print("Please pick EITHER an obsid or a row_number to search by, not both")
+	
+	if row_number is not None and obsid is None:
+		prev_row = np.where(np.argsort(all_obs) == row_number -1 )
+		prev_index = prev_row[0][0]
+		
+		if prev_index == 0:
+			return None
+			
+		return previous
 
-	return previous_obsid
+	if row_number is None and obsid is not None: 
+		sorted_obsid_indices = np.argsort(all_obsid)
+		
+		previous_obsid = None
+	
+		for current_index in sorted_obsid_indices:
+			current_obsid = all_obsid[current_index]
+			if current_obsid >= obsid:
+				break
+			previous_obsid = float(current_obsid)
+	
+		return previous_obsid
