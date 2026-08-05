c7e22cfc60292afd6feb1e51fe83b73ede2888ca
hirata10 <hirata.10@osu.edu>
2026-08-05 15:11:03 -0400

    Refactor docstring in previous_obsid function
    Updated docstring for clarity and consistency.

--- src/roman_hlis_l2_driver/persistence/persistence_flag.py
+++ src/roman_hlis_l2_driver/persistence/persistence_flag.py
@@ -5,30 +5,39 @@ import numpy as np
 from astropy.io import fits as f
 
 def previous_obsid(inputfile: str, row_number = None, obsid = None):
-	""" Given an observation ID (or a row number) and a path to a FITS file,
-        returns the ID (or row number) of the observation that came before it
-		in the file.
+	"""
+	Given an observation ID (or a row number) and a path to a FITS file,
+    returns the ID (or row number) of the observation that came before it
+	in the file.
+
+    Exactly one of `row_number` or `obsid` must be provided.
+
     Parameters
-    ---------
+    ----------
     inputfile: str or str-like
         Path to observation table fits file.
-    row_number: int
+    row_number: int, optional
         Unsorted row number, or index, of an observation within the fits file.
-    obsid: int
+    obsid: int, optional
         Observation ID, taken as the modified Julian date.
-    Returns
+
+	Returns
     -------
-    obsid_prev: int
-        Observation ID of the previous observation to the one passed in.
-    prev_row: int
-        Row number of the observation previous to the row_number passed,
+    int
+	    One of either "obsid_prev" or "prev_row", depending on whether `obsid` or
+	    `row_number` is provided:
+
+		- ``obsid_prev`` : Observation ID of the previous observation to the one passed in.
+        - ``prev_row`` : Row number of the observation previous to the row_number passed,
 		were the rows sorted by date.
+
 	"""
+
 	in_file = f.open(inputfile)
 	all_obs = in_file[1].data["date"]
 
 	if row_number is not None and obsid is not None:
-		print("Please pick EITHER an obsid or a row_number to search by, not both")
+		raise ValueError("Please pick EITHER an obsid or a row_number to search by, not both")
 	
 	if row_number is not None and obsid is None:
 		sorted_index = int(np.where(np.argsort(all_obs) == row_number)[0][0])
