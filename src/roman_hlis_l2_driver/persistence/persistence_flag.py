9d9fac6acb23c5f9e9642224bb3dfcce333ee079
THT <43194082+aeharbo@users.noreply.github.com>
2026-08-12 13:02:48 -0400

    Correct previous_obsid call in persistence_flag.py
    Fix backup step logic to use current id instead of decrementing.

--- src/roman_hlis_l2_driver/persistence/persistence_flag.py
+++ src/roman_hlis_l2_driver/persistence/persistence_flag.py
@@ -89,7 +89,8 @@ def get_prev_obs(obstable: str, nbackup=1, id=None):
     current_obs = all_obs[id]
     delta_t = None
     for backup_step in range(nbackup):
-        prev_id = previous_obsid(inputfile=obstable, row_number=(id - backup_step))
+        prev_id = previous_obsid(inputfile=obstable, row_number=id)
+        id = prev_id
 
     if prev_id is not None:
         prev_obs = all_obs[prev_id]
