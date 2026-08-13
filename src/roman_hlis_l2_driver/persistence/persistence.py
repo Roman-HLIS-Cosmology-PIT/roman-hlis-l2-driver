993b149daf35c35acac6d62d92eebb3e3751a964
THT <43194082+aeharbo@users.noreply.github.com>
2026-08-12 23:55:43 -0400

    Modify loop to break instead of continue

--- src/roman_hlis_l2_driver/persistence/persistence.py
+++ src/roman_hlis_l2_driver/persistence/persistence.py
@@ -73,7 +73,7 @@ def run(cfg: str, l2dir: str, delta_t_prime_max = 1200.0, signal_threshold = 200
       l2_directory = Path(cfg_file["INDATA"][0][:-3]+"/")
       for l2_file in l2_directory.iterdir():
         print(f"looking at file: {l2_file.name} in the L2 directory")
-        continue
+        break
 
   
   
