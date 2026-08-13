e83ab8524b79297679e9e2a2a08624e3a984765a
THT <43194082+aeharbo@users.noreply.github.com>
2026-08-12 22:52:01 -0400

    Refactor file handling and add logging in persistence.py
    Updated file handling and logging in persistence.py to improve clarity and functionality.

--- src/roman_hlis_l2_driver/persistence/persistence.py
+++ src/roman_hlis_l2_driver/persistence/persistence.py
@@ -26,33 +26,46 @@ def run(cfg, delta_t_prime_max = 1200.0, signal_threshold = 20000.0, l2dir: str)
     prev_id: int
         #
     """
-  persistence_mask = np.full((4088,4088),False)
-  
+  # open the config file
   with open(cfg, 'r') as file:
     cfg_file = json.load(file)
+    print("loaded config file")
 
+  
   obs_file_path = cfg_file["OBSFILE"]
   obs_file = f.open(obs_file_path)
-  obs_list = obs_file[1].data["date"]
+  print("loaded fits file")
+  date_list = obs_file[1].data["date"]
   exptime_list = obs_file[1].data["exptime"]
   
   file_directory = Path(cfg_file["INDATA"][0])
+  print(f"file_directory points to: {file_directory}")
 
   for file in file_directory.iterdir():
+    persistence_mask = np.full((4088,4088),False)
+    
     search_format = r'(\d+)_(\d+)\.asdf$'
-    matches = re.search(search_format,cfg["OBSFILE"])
+    matches = re.search(search_format,file)
     obsid = matches[1]
     sca = matches[2]
+
+    print(f"this file is: {}, id: {}, sca: {}")
 
     delta_t = 0
     delta_t_prime = 0
     prev_ids = []
+
+    break
+
+  
+    
     while delta_t_prime <= delta_t_prime_max:
-      prev_id, delta_t = pf.get_prev_obs(obs_list,id=obsid)
+      prev_id, delta_t = pf.get_prev_obs(date_list,id=obsid)
       # delta_t returned in days, convert to seconds
       delta_t *= 86400 # (24*60*60) to go from days to secs
       delta_t_prime = delta_t + exptime[prev_id]
-      prev_ids.append(prev_id)
+      l2_directory = Path(cfg_file["INDATA"][0][:-3]+"/")
+      for file in l2_directory.iterdir():
 
   
   
